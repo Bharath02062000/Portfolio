dada466acd54008e76d96b4a79c131f4ee0891ab
Bharath kumar <127566891+Bharath02062000@users.noreply.github.com>
2024-12-08 18:06:56 +0530

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
       <div className={styles.projectsContainer}>
       <ProjectCard
           src={vehk}
-          link="https://vehk.in"
+          link="www.vehk.in"
           h3="vehk.in"
           p="Vehk Website"
         />
